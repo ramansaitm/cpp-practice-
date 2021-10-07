5d4b6a19e3cdb4dbb512a7f9e38a067867084ca5
Raman kumar <92011450+ramansaitm@users.noreply.github.com>
2021-10-08 00:59:50 +0530

    for printing hello world

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -2,7 +2,7 @@
 using namespace std; // this means we are using standard space
 
 int main(){ // start
-	cout<<"Hello World"<<'\n'<<"Kartik Mathur"<<'\n';
+	cout<<"Hello World"<<'\n'<<"Raman Kumar"<<'\n';
 
 	// cout<<"Hello World";
 	// cout<<'\n'; '\n' == endl
