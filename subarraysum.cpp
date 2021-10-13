592c672c67bf053efbc72cf03fd3f1a0a3163681
Raman kumar <92011450+ramansaitm@users.noreply.github.com>
2021-10-13 15:42:40 +0530

    sum of sub array

---
+++ subarraysum.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+using namespace std;
+
+void fun(int arr[],int    j,int p)
+{
+for(int i=p;i<=j;i++)
+{
+  cout<<arr[i]<<" ";
+}
+
+}
+
+int main ()
+{
+  int n;
+  cin>>n;
+  int arr[n];
+ int sum=0; 
+  for(int i=0;i<n;i++)
+  {
+      
+      cin>>arr[i];
+      
+  }
+   //sub array loic
+   cout<<"subarray is :"<<(n*(n+1))/2<<endl;
+   for(int i=0;i<n;i++)
+   {
+        int current=0;
+        
+       for(int j=i;j<n;j++)
+       {
+           
+        current+=arr[j];
+        sum+=current;
+        cout<<current<< " ";
+       }
+       
+     cout<<endl;
+   }
+   cout<<sum;
+    return 0;
+}
